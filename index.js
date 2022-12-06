3babf97d6498552b2f2bfa2c77586f3f0f348d9e
SanyuPark <sangwp97@naver.com>
2022-12-06 21:47:43 +0900

    음악 재생 추가-재생만됨..

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
-/*
-// 음악재생
-document.querySelector('.btn1').addEventListener('click', function () {
-  const audio1 = new Audio('#.mp3');
-  audio1.loop = false; //반복재생하지않음
-  audio1.volume = 0.5; //음량 설정
-  audio1.play(); // "#.mp3 재생"
-});
-// 음악 클릭시 음악이름 출력
-const btn1Click = document.getElementById('play-song-name');
-function btn1Click(onclick) {
-  console.log((document.getElementById('play-song-name').innerHTML = '200%'));
-}
-*/
-
-// const musicName = songInfo(
-// document.getElementsByClassName('songName').innerHTML,
-// );
-
 // 음악 리스트 클릭 시... 화면에 음악이름 및 앨범사진 표출
-function handleSongCardClick(songName, imgSrc) {
+
+function handleSongCardClick(songName, imgSrc, audioId) {
   // 음악 이름 변경
-  const selectedSongTitle = (document.getElementsByTagName(songName).innerHTML =
-    songName);
-  document.querySelector('.playing-music').innerHTML = selectedSongTitle;
-  document.querySelector('.playing-song-name').innerHTML = selectedSongTitle;
+  document.querySelector('.playing-music').innerHTML = songName;
+  document.querySelector('.playing-song-name').innerHTML = songName;
   // 음악 사진 변경
-  const selectedSongPicture = (document.getElementsByTagName(imgSrc).src =
-    imgSrc);
-  document.querySelector('#playing-album').src = selectedSongPicture;
+  document.querySelector('#playing-album').src = imgSrc;
+  // 음악 실행
+  const playAudio = document.getElementById(audioId);
+  playAudio.play();
 }
 
 //좋아요클릭
-
-const heartCount = document.getElementById('hearts');
-function heartClick() {
-  for (var i = 0; i < heartCount.classList.length; i++) {
-    if (heartCount.classList[i].indexOf('black') >= 0) {
-      heartCount.classList.remove('black');
-      heartCount.classList.add('red');
-    } else {
-      heartCount.classList.remove('red');
-      heartCount.classList.add('black');
-    }
-  }
+function handleHeartClick() {
+  const heartButton = document.getElementById('hearts');
+  heartButton.classList = heartButton.className === 'black' ? 'red' : 'black';
 }
